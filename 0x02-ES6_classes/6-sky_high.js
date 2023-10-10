ae2a6798492a8240621dd098fe31abbcac7cd227
Aseye-1 <aboraseye@gmail.com>
2023-10-10 17:23:32 +0000

    updated

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,29 +1,16 @@
-// Import the Building class from 5-building.js
 import Building from './5-building';
 
-// Define the SkyHighBuilding class
-class SkyHighBuilding extends Building {
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
   }
 
-  // Getter for sqft attribute
-  get sqft() {
-    return super.sqft;
-  }
-
-  // Getter for floors attribute
   get floors() {
     return this._floors;
   }
 
-  // Override the evacuationWarningMessage method
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors.`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
-
-// Export the SkyHighBuilding class
-export default SkyHighBuilding;
-
